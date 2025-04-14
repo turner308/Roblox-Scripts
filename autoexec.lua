3574592c10f4b5d011b1fd11c0880677be3710cb
aturner <69327501+turner308@users.noreply.github.com>
2025-04-14 17:43:20 -0500

    Create autoexec.lua

---
+++ autoexec.lua
@@ -0,0 +1,48 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+local LogService = game:GetService("LogService")
+
+--Anti AFK
+for _, v in next, getconnections(LocalPlayer.Idled) do
+    v:Disable()
+end
+--
+
+--Disable Print LogService Detection
+hookfunc(LogService.MessageOut, function() return end)
+--
+
+local KeyMap = {
+    ["RightControl"] = {
+        ["KeypadOne"] = function()
+            --Hydroxide
+            getgenv().getConstants = function() return {} end
+            getgenv().getProtos = function() return {} end
+            loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Upbolt/Hydroxide/revision/init.lua"))()
+            loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Upbolt/Hydroxide/revision/ui/main.lua"))()
+        end,
+        ["KeypadTwo"] = function()
+            --IY
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))()
+        end,
+        ["KeypadThree"] = function()
+            --Frosthook
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/turner308/Roblox-Scripts/refs/heads/master/Frosthook.lua"))()
+        end
+    }
+}
+
+UserInputService.InputBegan:Connect(function(InputObject, GPE)
+    if GPE then return end
+
+    for keyHold, keyBinds in next, KeyMap do
+        if keyHold == "" or keyHold:lower() == "none" or UserInputService:IsKeyDown(keyHold) then
+            for keyBind, Func in next, keyBinds do
+                if InputObject.KeyCode.Name == keyBind then
+                    Func()
+                end
+            end
+        end
+    end
+end)
