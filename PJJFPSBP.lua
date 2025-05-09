8e2e054287f9ffa41696032e7609b1067434c753
aturner <69327501+turner308@users.noreply.github.com>
2025-05-09 09:04:50 -0500

    Create PJJFPSBP.lua

---
+++ PJJFPSBP.lua
@@ -0,0 +1,19 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+coroutine.wrap(function()
+    LocalPlayer.PlayerGui:WaitForChild("fpsKickWarning", math.huge):Destroy()
+    LocalPlayer.PlayerScripts:WaitForChild("fpsScript", math.huge):Destroy()
+    LocalPlayer.PlayerScripts:WaitForChild("FakeWeld", math.huge):Destroy()
+end)()
+
+local KickHook
+KickHook = hookmetamethod(game, "__namecall", function(self, ...)
+    local namecallmethod = getnamecallmethod()
+
+    if namecallmethod:lower() == "kick" then
+        return
+    end
+
+    return KickHook(self, ...)
+end)
